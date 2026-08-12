ed00cc6c6191efb0f9faa2383f1dbec9bf641f42
Vaishnavi-max <143889962+Vaishnavi-max@users.noreply.github.com>
2026-08-12 12:06:58 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int n;
+    vector<int>dp;
+    // int func(int i,vector<int>&nums){
+    //     if(i>=n){
+    //         return 0;
+    //     }
+    //     if(i==n-1){
+    //         return nums[n-1];
+    //     }
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+    //     int take=nums[i]+func(i+2,nums);
+    //     int not_take=0+func(i+1,nums);
+    //     return dp[i]=max(take,not_take);
+    // }
+    int rob(vector<int>& nums) {
+        n=nums.size();
+        dp.resize(n+1,-1);
+        dp[n]=0;
+        dp[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            int take=nums[i]+dp[i+2];
+            int not_take=0+dp[i+1];
+            dp[i]=max(take,not_take);
+        }
+        return dp[0];
+    }
+};
